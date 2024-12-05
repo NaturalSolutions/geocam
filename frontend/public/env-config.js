87fb8b80ae8bdce0fc7a25afb52e55488bce2460
Mathilde Leclerc <mathilde_leclerc@natural-solutions.eu>
2024-12-05 16:54:57 +0100

    clean files

--- frontend/public/env-config.js
+++
@@ -1,6 +0,0 @@
-window._env_ = {
-  REACT_APP_API_PATH: "http://localhost:443/api/v1",
-  REACT_APP_KEYCLOAK_CLIENT_URL: "http://localhost:443/auth",
-  REACT_APP_KEYCLOAK_CLIENT_REALM: "test-ecosecrets",
-  REACT_APP_KEYCLOAK_CLIENT_CLIENT_ID: "frontend",
-}
--- frontend/src/components/imageList.tsx
+++ frontend/src/components/imageList.tsx
@@ -7,17 +7,17 @@ import Dropzone from "react-dropzone";
 import { Grid, Stack, Typography, capitalize } from "@mui/material";
 import { useParams } from "react-router-dom";
 import { FilesService } from "../client";
-import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
 import { useTranslation } from "react-i18next";
 import ButtonsYesNo from "./common/buttonsYesNo";
 
 const ImageList: FC<{}> = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
   const [files, setFiles] = useState<any[]>([]);
-  const { projects, updateListFile, setCurrentDeployment, deploymentData, } =
+  const { projects, updateListFile, setCurrentDeployment, deploymentData } =
     useMainContext();
   let params = useParams();
-  
+
   useEffect(() => {
     (async () => {
       setCurrentDeployment(Number(params.deploymentId));
@@ -26,11 +26,11 @@ const ImageList: FC<{}> = () => {
 
   const save = () => {
     for (const file of files) {
-      FilesService
-      .uploadFileFilesUploadDeploymentIdPost(Number(params.deploymentId), { file })
-      .then((res) => {
+      FilesService.uploadFileFilesUploadDeploymentIdPost(
+        Number(params.deploymentId),
+        { file }
+      ).then((res) => {
         updateListFile();
-       
       });
     }
     clear();
@@ -47,9 +47,7 @@ const ImageList: FC<{}> = () => {
 
   const dropZoneDisplayText = () => {
     if (files.length === 0) {
-      return (
-        <p>{capitalize(t("deployments.drop_files"))}</p>
-      );
+      return <p>{capitalize(t("deployments.drop_files"))}</p>;
     } else {
       return <p>{files.map((f) => f.name).join(", ")}</p>;
     }
@@ -59,21 +57,20 @@ const ImageList: FC<{}> = () => {
     <>
       {deploymentData ? (
         <Stack spacing={2}>
-          <Typography variant="h6" sx={{ mb:2}}>{capitalize(t("projects.import_media"))}</Typography>
+          <Typography variant="h6" sx={{ mb: 2 }}>
+            {capitalize(t("projects.import_media"))}
+          </Typography>
           <Dropzone onDrop={loadFile} multiple maxFiles={10}>
             {({ getRootProps, getInputProps }) => (
               <section id="dropzone">
                 <div {...getRootProps()}>
                   <input {...getInputProps()} />
-                  <Grid container direction="column" alignItems='center'>
+                  <Grid container direction="column" alignItems="center">
                     <Grid item>
                       <CameraAltIcon fontSize="large" />
                     </Grid>
-                    <Grid item>
-                      {dropZoneDisplayText()}
-                    </Grid>
+                    <Grid item>{dropZoneDisplayText()}</Grid>
                   </Grid>
-
                 </div>
               </section>
             )}
@@ -84,10 +81,10 @@ const ImageList: FC<{}> = () => {
             alignItems="center"
             spacing={2}
           >
-            <ButtonsYesNo 
-              onYes={ save } 
-              onNo={ clear } 
-              yesContent={ capitalize(t("main.save"))} 
+            <ButtonsYesNo
+              onYes={save}
+              onNo={clear}
+              yesContent={capitalize(t("main.save"))}
               noContent={capitalize(t("main.cancel"))}
             />
           </Stack>
