861327baef34e8983ef3ea1545109751ab4e692f
Mathilde Leclerc <mathilde_leclerc@natural-solutions.eu>
2024-12-13 15:40:02 +0100

    wip date component in frontend

--- frontend/src/components/annotation/ObservationTab.tsx
+++ frontend/src/components/annotation/ObservationTab.tsx
@@ -1,79 +1,95 @@
-import { capitalize, FormControlLabel, Switch } from "@mui/material";
+import { capitalize, FormControlLabel, Switch, Stack } from "@mui/material";
 import ObservationForm from "./ObservationForm";
 import TabPanel from "../tabPanel";
 import ButtonStatus from "../common/buttonStatus";
-import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded';
-import HelpRoundedIcon from '@mui/icons-material/HelpRounded';
+import CheckCircleRoundedIcon from "@mui/icons-material/CheckCircleRounded";
+import HelpRoundedIcon from "@mui/icons-material/HelpRounded";
 import { useTranslation } from "react-i18next";
 import { useAnnotationContext } from "../../contexts/annotationContext";
 import { Annotation } from "../../client";
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import TextField from "@mui/material/TextField";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import frLocale from "date-fns/locale/fr";
 
 interface ObservationTabProps {
-    valueTab: number;
-    index: number;
-};
+  valueTab: number;
+  index: number;
+}
 
+const ObservationTab: FC<ObservationTabProps> = ({ valueTab, index }) => {
+  const { t } = useTranslation();
+  const { date, setDate } = useAnnotationContext();
+  const { observations, annotated, treated, checked, handleCheckChange } =
+    useAnnotationContext();
+  console.log(date, typeof date);
 
-const ObservationTab: FC<ObservationTabProps> = ({
-    valueTab,
-    index
-}) => {
-    const { t } = useTranslation();
-    
-    const { 
-        observations,
-        annotated,
-        treated,
-        checked,
-        handleCheckChange,
-    } = useAnnotationContext();
-
-    return(
-        <TabPanel 
-            valueTab={ valueTab } 
-            index={ index }
-        >
-            <span className="info-annotation-ctn">
-            { treated ?
-                <ButtonStatus 
-                    icon={ <CheckCircleRoundedIcon sx={{ color: "#4CAF50" }} /> } 
-                    title={ capitalize(t("annotations.media_processed_manually")) } 
-                    stylClassButton="valid" 
-                /> : (
-                annotated ?
-                <ButtonStatus 
-                    icon={ <HelpRoundedIcon sx={{ color: "#FF9800" }} /> } 
-                    title={ capitalize(t("observations.not_saved")) } 
-                    stylClassButton="info" 
-                /> :
-                <ButtonStatus 
-                    icon={ <HelpRoundedIcon sx={{ color: "#F44336" }} /> } 
-                    title={ capitalize(t("annotations.media_not_processed")) } 
-                    stylClassButton="warning" 
-                />
+  return (
+    <TabPanel valueTab={valueTab} index={index}>
+      <Stack
+        direction="row"
+        spacing={2}
+        alignItems="center"
+        justifyContent="space-between"
+        style={{ marginBottom: "16px" }}
+      >
+        <LocalizationProvider dateAdapter={AdapterDateFns} locale={frLocale}>
+          <DateTimePicker
+            label={capitalize(t("medias.date_time_field"))}
+            value={date}
+            onChange={(newValue) => setDate(newValue)}
+            ampm={false}
+            inputFormat="yyyy/MM/dd HH:mm:ss" // Ajout de l'affichage des secondes
+            renderInput={(params) => (
+              <TextField {...params} sx={{ width: "220px" }} />
             )}
-            <FormControlLabel
-                id="switch-empty-control"
-                control={
-                <Switch
-                    id="switch-empty"
-                    checked={ checked }
-                    onChange={ handleCheckChange }
-                />
-                }
-                label={ capitalize(t("annotations.empty_media")) }
-            />
-            </span>
+          />
+        </LocalizationProvider>
+        {/* <span className="info-annotation-ctn"> */}
+        {treated ? (
+          <ButtonStatus
+            icon={<CheckCircleRoundedIcon sx={{ color: "#4CAF50" }} />}
+            title={capitalize(t("annotations.media_processed_manually"))}
+            stylClassButton="valid"
+          />
+        ) : annotated ? (
+          <ButtonStatus
+            icon={<HelpRoundedIcon sx={{ color: "#FF9800" }} />}
+            title={capitalize(t("observations.not_saved"))}
+            stylClassButton="info"
+          />
+        ) : (
+          <ButtonStatus
+            icon={<HelpRoundedIcon sx={{ color: "#F44336" }} />}
+            title={capitalize(t("annotations.media_not_processed"))}
+            stylClassButton="warning"
+          />
+        )}
 
-            {observations?.map((observation: Annotation, index: number) => (
-                <ObservationForm 
-                    key={ observation.id }
-                    index={ index + 1 }
-                    observation={ observation } 
-                />
-            ))}
-        </TabPanel >
-    )
+        <FormControlLabel
+          id="switch-empty-control"
+          control={
+            <Switch
+              id="switch-empty"
+              checked={checked}
+              onChange={handleCheckChange}
+            />
+          }
+          label={capitalize(t("annotations.empty_media"))}
+        />
+        {/* </span> */}
+      </Stack>
+
+      {observations?.map((observation: Annotation, index: number) => (
+        <ObservationForm
+          key={observation.id}
+          index={index + 1}
+          observation={observation}
+        />
+      ))}
+    </TabPanel>
+  );
 };
 export default ObservationTab;
